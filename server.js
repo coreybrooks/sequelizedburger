c9b9517ca29ee5438952aca41a4773c8a380b16a
Corey <coreybrooks2@gmail.com>
2017-05-08 14:23:37 -0500

    attempting to adjust file structure to match 2-sequelize-create-read

--- server.js
+++ server.js
@@ -1,15 +1,11 @@
 var express = require("express");
 var bodyParser = require("body-parser");
-var methodOverride = require("method-override");
 
 var app = express();
 var PORT = process.env.NODE_ENV || 8080;
 
 // Requiring our models for syncing
 var db = require("./models");
-
-
-
 
 // Sets up the Express app to handle data parsing
 app.use(bodyParser.json());
@@ -18,14 +14,10 @@ app.use(bodyParser.text());
 app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
 // Serve static content for the app from the "public" directory in the application directory.
-app.use(express.static(process.cwd() + "/public"));
+app.use(express.static("./public"));
 
 require("./routes/html-routes.js")(app);
 require("./routes/api-routes.js")(app);
-
-
-// Override with POST having ?_method=DELETE
-app.use(methodOverride("_method"));
 
 // Set Handlebars.
 var exphbs = require("express-handlebars");
