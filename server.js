66dd78185c20df1e5c7716e80487c07324d810e3
Corey <coreybrooks2@gmail.com>
2017-05-09 18:54:26 -0500

    fully functional, still need to update to heroku

--- server.js
+++ server.js
@@ -1,33 +1,39 @@
 var express = require("express");
 var bodyParser = require("body-parser");
-
-var app = express();
-var PORT = process.env.NODE_ENV || 8080;
+var methodOverride = require("method-override");
 
 // Requiring our models for syncing
 var db = require("./models");
 
-// Sets up the Express app to handle data parsing
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.text());
-app.use(bodyParser.json({ type: "application/vnd.api+json" }));
-
+var app = express();
 // Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static("./public"));
 
-require("./routes/html-routes.js")(app);
-require("./routes/api-routes.js")(app);
 
+
+// Sets up the Express app to handle data parsing
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+
+// override with POST having ?_method=DELETE
+app.use(methodOverride("_method"));
 // Set Handlebars.
 var exphbs = require("express-handlebars");
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
+var routes = require("./controllers/burgers_controller.js");
 
+app.use("/", routes);
+app.use("/update", routes);
+app.use("/create", routes);
+
+var PORT = process.env.NODE_ENV || 8080;
 // Syncing our sequelize models and then starting our express app
-db.sequelize.sync({ force: true }).then(function() {
+db.sequelize.sync({ force: false }).then(function() {
   app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
   });
